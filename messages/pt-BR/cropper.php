30866bad85d71e46d4ef2323c202f23f39e872f5
Dan Perjar <dperjar@gmail.com>
2025-01-28 12:50:58 -0500

    Create pt-BR language support file

---
+++ messages/pt-BR/cropper.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'DEFAULT_LABEL' => 'Clique e selecione ou arraste um arquivo de imagem aqui',
+    'CROP_PHOTO' => 'Cortar imagem',
+    'DELETE_PHOTO' => 'Excluir imagem',
+    'ERROR_CAN_NOT_UPLOAD_FILE' => 'Não foi possível fazer o upload do arquivo',
+    'MISSING_ATTRIBUTE' => 'O atributo "{attribute}" deve ser especificado',
+    'ONLY_POST_REQUEST' => 'Somente requisições POST são permitidas',
+    'UPLOAD_ANOTHER_PHOTO' => 'Enviar outra foto',
+    'TOO_BIG_ERROR' => 'O tamanho do arquivo excede o limite permitido ({size} Mb)',
+    'EXTENSION_ERROR' => 'São permitidos apenas os seguintes formatos de arquivo: {formats}'
+];
